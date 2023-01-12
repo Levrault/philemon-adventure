f48836410a8d36ef74cf03d598260d93471d02d7
levrault <lucf.langis@gmail.com>
2023-01-12 13:29:14 -0500

    feat(player): add cooldown between shot

--- src/Commons/Combat/Muzzle.gd
+++ src/Commons/Combat/Muzzle.gd
@@ -2,8 +2,23 @@ extends Position2D
 
 export(Resource) var projectile_data
 
+var timer:Timer = null
+
+
+func _ready() -> void:
+	if projectile_data.cooldown > 0:
+		timer = Timer.new()
+		timer.one_shot = true
+		timer.wait_time = projectile_data.cooldown
+		add_child(timer)
+
 
 func shoot(res: ProjectileResource) -> void:
+	if timer != null:
+		if not timer.is_stopped():
+			return
+		timer.start()
+		
 	var instance = projectile_data.scene.instance()
 	GameManager.add_child_to_root(instance, global_position)
 	instance.init(res)
--- src/Resources/ProjectileResourceBase.gd
+++ src/Resources/ProjectileResourceBase.gd
@@ -1,5 +1,6 @@
 extends DamageSourceResourceBase
 class_name ProjectileResource
 
-export var scene: PackedScene
 export var speed: float = 200.0
+export var cooldown: float = 0.00
+export var scene: PackedScene
