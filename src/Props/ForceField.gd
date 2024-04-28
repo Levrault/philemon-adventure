10ca79e8806c3b9d37132577b6eb6e0d51e8046e
levrault <lucf.langis@gmail.com>
2024-04-28 14:07:50 -0400

    fix bugs

--- src/Props/ForceField.gd
+++ src/Props/ForceField.gd
@@ -13,6 +13,7 @@ func _ready() -> void:
 	area.connect("body_entered", self, "_on_Body_entered")
 	area.connect("body_exited", self, "_on_Body_exited")
 	set_type(type)
+	$CardSprite.visible = false
 	if GameManager.is_card_upgrade_status_unlocked(type):
 		is_active = false
 
@@ -21,15 +22,19 @@ func set_type(p_type: int) -> void:
 	type = p_type
 	if type == GameManager.Card.LVL_1:
 		$Sprite.modulate = Color("#a3ce27")
+		$CardSprite.frame = 1
 		return
 	if type == GameManager.Card.LVL_2:
 		$Sprite.modulate = Color("#007899")
+		$CardSprite.frame = 3
 		return
 	if type == GameManager.Card.LVL_3:
 		$Sprite.modulate = Color("#be2633")
+		$CardSprite.frame = 2
 		return
 	if type == GameManager.Card.LVL_4:
 		$Sprite.modulate = Color("#000000")
+		$CardSprite.frame = 0
 		return
 
 
@@ -46,17 +51,19 @@ func _on_Card_unlocked(card_type) -> void:
 
 
 func _on_Body_entered(body: Node) -> void:
-	if is_active:
-		return
 	if not body is Player:
+		return
+	if is_active:
+		$CardAnimationPlayer.play("on")
 		return
 	$AnimationPlayer.play("off")
 
 
 func _on_Body_exited(body: Node) -> void:
-	if is_active:
-		return
 	if not body is Player:
+		return
+	if is_active:
+		$CardAnimationPlayer.play("off")
 		return
 	$AnimationPlayer.play_backwards("off")
 	$AnimationPlayer.queue("on")
